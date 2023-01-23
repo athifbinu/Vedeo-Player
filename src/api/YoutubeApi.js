83bced5761562be799d79fed0a5ee505a23aeac1
lenovo <Athif binu>
2023-01-23 13:05:31 +0530

    lets start

---
+++ src/api/YoutubeApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const KEY = 'AIzaSyBdsNsdx2EzmNu_FCwolTqsVDU3ijD2tDk';  //api key
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',   //base url
+    params:{
+        part:'snippet',
+        type: "video",
+        maxResults:5,
+        key:KEY,
+
+    }
+    
+})
+
